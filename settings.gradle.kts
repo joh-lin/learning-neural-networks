2d5f87d0a9aae47780cd293db4ddc853029b74b8
Johannes <johannes.lingk@gmail.com>
2022-08-02 23:12:56 +0200

    Recreated project.
    Due to errors caused by IntelliJ the project had to be deleted and newly
    created. The libraries GSON and LJML were also added.

--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-
-rootProject.name = "learning-neural-networks"
-
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+
+import org.ejml.simple.SimpleMatrix
+
+
+fun main() {
+    /*val firstMatrix = SimpleMatrix(
+        arrayOf(
+            doubleArrayOf(1.0, 5.0),
+            doubleArrayOf(2.0, 3.0),
+            doubleArrayOf(1.0, 7.0)))
+
+    val secondMatrix = SimpleMatrix(
+        arrayOf(
+            doubleArrayOf(1.0, 2.0, 3.0, 7.0),
+            doubleArrayOf(5.0, 2.0, 8.0, 1.0)))
+
+    val expected = SimpleMatrix(
+        arrayOf(
+            doubleArrayOf(26.0, 12.0, 43.0, 12.0),
+            doubleArrayOf(17.0, 10.0, 30.0, 17.0),
+            doubleArrayOf(36.0, 16.0, 59.0, 14.0)
+        )
+    )
+
+    val actual = firstMatrix.mult(secondMatrix)
+
+    actual.print()*/
+}
